ecec6a114dde719836b6178f75304667dc4bf660
MexicanCacti <JosephMRDominguez@gmail.com>
2022-10-04 11:50:09 -0700

    Milo Dominguez

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import random
+numbers = [] * 10
+for i in range(10):
+    numbers.append(random.randint(0,100))
 
-# ******************************
-# Make your Code
-# ******************************
+min = min(numbers)
+min_index = numbers.index(min)
+
+print(f"Min: {min}")
+print(f"Min Index: {min_index}")
